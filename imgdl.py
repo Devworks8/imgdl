713e5bb6d155b4cf83ee4737b3c7febb3d164405
Devworks8 <devworks8@gmail.com>
2017-01-18 15:20:35 -0500

    Added threading for shell.

--- imgdl.py
+++ imgdl.py
@@ -4,15 +4,24 @@ Version: 0.1
 Description: Image downloader for various services. I.e. Facebook, Pintress, etc.
 """
 
+from threading import Thread
+
 import configuration
+import robot
 
 
-def main():
+def main(**kwargs):
     config = configuration.Config()
     settings = config.get_settings()
     services = config.get_services()
-    print(settings)
-    print(services)
+    if not kwargs:
+        print("must process kwargs")
+    shell = robot.Shell(**kwargs)
+    cmd_loop_thread = Thread(target=shell.run)
+    cmd_loop_thread.daemon = True
+    cmd_loop_thread.start()
+
+    shell.cmdloop()
 
 
 if __name__ == "__main__":
