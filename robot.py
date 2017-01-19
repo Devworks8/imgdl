d19f5414bc6e62e4701f3cb340d494c3cc504371
Devworks8 <devworks8@gmail.com>
2017-01-19 14:46:25 -0500

    Started implementing the core command line parser.

--- robot.py
+++ robot.py
@@ -5,7 +5,6 @@ Description: Command shell terminal.
 """
 
 import cmd
-import argparse
 
 __version__ = '0.1'
 
@@ -13,26 +12,28 @@ __version__ = '0.1'
 class Shell(cmd.Cmd):
     def __init__(self,
                  context=None,
-                 prompt_name='imgdl',
-                 **kwargs):
+                 prompt_name='imgdl'):
         super().__init__()
         self.stdout.write('Imgdl {}\n'.format(__version__))
-        self.stdout.write("Type \"help\" for command line help or "
-                          "\"commands\" for bot commands\n\n")
+        self.stdout.write("Type \"help\" for command line help\n\n")
         self.prompt = prompt_name + ': '
         self.misc_header = "Commands"
-        self.running = False
 
-    # TODO: Fix me.
-    def runparser(self):
-        parser = argparse.ArgumentParser(description='Image Downloader', prog='Imgdl')
-        parser.add_argument('start', nargs=1, metavar='<service>', dest='command', help='Start a service.')
-        parser.add_argument('stop', nargs=1, metavar='<service>', dest='command', help='Stop a service')
-        parser.add_argument('restart', nargs=1, metavar='<service>', dest='command', help='Restart a service')
-        self.running = True
-        return parser.parse_args()
+    def do_start(self, args):
+        print('start service')
+
+    def do_stop(self, args):
+        print('stop service')
+
+    def do_restart(self, args):
+        print('restart service')
+
+    def do_quit(self, args):
+        print('Quitting.')
+        raise SystemExit
+
+    def emptyline(self):
+        pass
 
     def run(self):
-        if not self.running:
-            args = self.runparser()
-        print('--' + cmd.Cmd.precmd(self, args.command[0]))
+        pass
