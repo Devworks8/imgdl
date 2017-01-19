7af2bc4ddc98b3fa48ad84dfc3c8bcd5412ad972
Devworks8 <devworks8@gmail.com>
2017-01-19 14:47:52 -0500

    Transferred argument parser from robot.py

--- imgdl.py
+++ imgdl.py
@@ -5,18 +5,34 @@ Description: Image downloader for various services. I.e. Facebook, Pintress, etc
 """
 
 from threading import Thread
+import argparse
 
 import configuration
 import robot
 
+__version__ = '0.1'
 
-def main(**kwargs):
+
+def runparser():
+    parser = argparse.ArgumentParser(description='Image Downloader', prog='Imgdl')
+    parser.add_argument('--gui', action='store_true', dest='gui', help='Start GUI.')
+    parser.add_argument('--debug', action='store_true', dest='debug', help='Enable debugging.')
+    parser.add_argument('--version', action='version', version='%(prog)s {}'.format(__version__))
+    return parser.parse_args()
+
+
+def main():
     config = configuration.Config()
     settings = config.get_settings()
     services = config.get_services()
-    if not kwargs:
-        print("must process empty kwargs")
-    shell = robot.Shell(**kwargs)
+
+    # Parse command line arguments.
+    args = runparser()
+    if args.gui:
+        print('Run GUI')
+    elif args.debug:
+        print('Run debugger, and enable logging')
+    shell = robot.Shell()
     cmd_loop_thread = Thread(target=shell.run)
     cmd_loop_thread.daemon = True
     cmd_loop_thread.start()
