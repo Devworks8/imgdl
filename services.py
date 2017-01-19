36c20fe4120c2f31bd46eb3df5125facd9930e89
Devworks8 <devworks8@gmail.com>
2017-01-19 17:03:43 -0500

    initial commit.

---
+++ services.py
@@ -0,0 +1,21 @@
+"""
+Name: services.py
+Version: 0.1
+Description: Services back-end.
+"""
+
+
+class Services:
+    def __init__(self,
+                 name=None,
+                 apikey=None,
+                 username=None,
+                 password=None):
+        """
+        Initialize service.
+        :param name: Service name.
+        :param apikey: Service API key.
+        :param username: Service account user name.
+        :param password: Service account password.
+        """
+        pass
