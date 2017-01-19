1b857f45aa8d2a5329ad47d98343d2a77738b5d1
Devworks8 <devworks8@gmail.com>
2017-01-19 17:24:40 -0500

    Expanded class.

--- pluginmanager.py
+++ pluginmanager.py
@@ -1,10 +1,45 @@
 """
 Name: Plugin Manager
 Version: 0.1
-Description: Allow the ability to start/stop/restart API services.
+Description: Allow the ability to start/stop/restart/query API services.
 """
 
 
 class Plugin:
     def __init__(self):
-        service
+        pass
+
+    def restart(self):
+        """
+        Restart the service.
+        :return:
+        """
+        pass
+
+    def start(self):
+        """
+        Start the service.
+        :return:
+        """
+        pass
+
+    def stop(self):
+        """
+        Stop the service.
+        :return:
+        """
+        pass
+
+    def query(self):
+        """
+        Conduct a query.
+        :return:
+        """
+        pass
+
+    def download(self):
+        """
+        Download images.
+        :return:
+        """
+        pass
