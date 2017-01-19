23f334135c813d509e58f581ad583596ef548601
Devworks8 <devworks8@gmail.com>
2017-01-18 19:15:07 -0500

    Command line argument parser started.

--- robot.py
+++ robot.py
@@ -5,6 +5,7 @@ Description: Command shell terminal.
 """
 
 import cmd
+import argparse
 
 __version__ = '0.1'
 
@@ -20,7 +21,18 @@ class Shell(cmd.Cmd):
                           "\"commands\" for bot commands\n\n")
         self.prompt = prompt_name + ': '
         self.misc_header = "Commands"
+        self.running = False
+
+    # TODO: Fix me.
+    def runparser(self):
+        parser = argparse.ArgumentParser(description='Image Downloader', prog='Imgdl')
+        parser.add_argument('start', nargs=1, metavar='<service>', dest='command', help='Start a service.')
+        parser.add_argument('stop', nargs=1, metavar='<service>', dest='command', help='Stop a service')
+        parser.add_argument('restart', nargs=1, metavar='<service>', dest='command', help='Restart a service')
+        self.running = True
+        return parser.parse_args()
 
     def run(self):
-        # self.stdout.write("imgdl: ")
-        self.stdout.flush()
+        if not self.running:
+            args = self.runparser()
+        print('--' + cmd.Cmd.precmd(self, args.command[0]))
